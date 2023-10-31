6917476ea034a12aac48c63b43048ebdf53585c4
gitxandert <alexandertoth96@yahoo.com>
2023-10-31 01:46:36 -0400

    Create script with basic math functions and some variables

---
+++ calculator.js
@@ -0,0 +1,16 @@
+const add=((a,b) => a+b);
+const subtract=((a,b) => a-b);
+const multiply=((a,b) => a*b);
+const divide=((a,b) => a/b);
+
+let operators={};
+operators.add=add;
+operators.subtract=subtract;
+operators.multiply=multiply;
+operators.divide=divide;
+
+let num1=0;
+let num2=0;
+let operator=''; 
+// use square brackets: operators[operator]
+
